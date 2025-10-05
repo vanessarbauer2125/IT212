2ba568b66bf9725a8bfd2595f05559570523e276
Vanessa Bauer <vanessarbauer12@gmail.com>
2025-10-05 10:51:44 -0700

    Add shapes

--- shapes/src/shapetests/CircleTest.java
+++ shapes/src/shapetests/CircleTest.java
@@ -6,14 +6,6 @@ import shapes.*;
 class CircleTest {
 
     @org.junit.jupiter.api.Test
-    void calculatePerimeter_withPostiveInt_ReturnsCorrectPerimeter() {
-        Shape circ = new Circle("circ", 10);
-        double perimeter = circ.calculatePerimeter();
-
-        Assertions.assertEquals(62.83185307179586, perimeter);
-    }
-
-    @org.junit.jupiter.api.Test
     void calculateArea_withPostiveInt_ReturnsCorrectArea() {
         Shape circ = new Circle("circ", 10);
         Assertions.assertEquals(314.1592653589793, circ.calculateArea());
